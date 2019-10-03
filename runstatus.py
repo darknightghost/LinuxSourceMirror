5b1cf000980bee56b7689c27ca69ebf113b095f4
wang_siyuan <darknightghost@gmail.com>
2019-10-04 00:32:01 +0800

    UTC:2019-10-03 16:32:01 :

--- runstatus.py
+++ runstatus.py
@@ -5,8 +5,8 @@ run = True
 exit_code = 0
 
 
-def exit(exit_code):
+def exit(_exit_code):
     global run
     global exit_code
     run = False
-    exit_code = exit_code
+    exit_code = _exit_code
