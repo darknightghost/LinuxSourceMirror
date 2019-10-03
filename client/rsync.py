e48625996dca221b2af1694f5cdba3160ed13b9b
wang_siyuan <darknightghost@gmail.com>
2019-10-03 23:38:28 +0800

    UTC:2019-10-03 15:38:28 : parse rsync exit code, change http web pages

--- client/rsync.py
+++ client/rsync.py
@@ -15,6 +15,27 @@ import signal
 class Client(client.Client, threading.Thread):
     """Rsync protocol."""
     SLEEP_TIME = 0.5
+    EXIT_CODES = {
+        1: "Syntax or usage error",
+        2: "Protocol incompatibility",
+        3: "Errors selecting input/output files, dirs",
+        4:
+        "Requested action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server.",
+        5: "Error starting client-server protocol",
+        6: "Daemon unable to append to log-file",
+        10: "Error in socket I/O",
+        11: "Error in file I/O",
+        12: "Error in rsync protocol data stream",
+        13: "Errors with program diagnostics",
+        14: "Error in IPC code",
+        20: "Received SIGUSR1 or SIGINT",
+        21: "Some error returned by waitpid()",
+        22: "Error allocating core memory buffers",
+        23: "Partial transfer due to error",
+        24: "Partial transfer due to vanished source files",
+        25: "The --max-delete limit stopped deletions",
+        30: "Timeout in data send/receive"
+    }
 
     def __init__(self, data_path, distros):
         """Constructor.
@@ -99,10 +120,19 @@ class Client(client.Client, threading.Thread):
                 del self.__tasks[distro]
 
             else:
-                logging.error("Rsync failed with exit code %d when " \
-                        "synchronizing distro \"%s\" from \"%s\"" % (exit_code,
-                            distro,
-                            config.config.distro_url(distro)))
+                try:
+                    logging.error("Rsync failed with exit code %d when " \
+                            "synchronizing distro \"%s\" from \"%s\" : %s" % (exit_code,
+                                distro,
+                                config.config.distro_url(distro),
+                                self.EXIT_CODES[exit_code]))
+
+                except KeyError:
+                    logging.error("Rsync failed with exit code %d when " \
+                            "synchronizing distro \"%s\" from \"%s\"" % (exit_code,
+                                distro,
+                                config.config.distro_url(distro)))
+
                 self.__tasks[distro] = self.mk_task(distro)
 
     def mk_task(self, name):
@@ -135,4 +165,7 @@ class Client(client.Client, threading.Thread):
         :return:    Command.
         :rtype:     :class:`str`
         """
-        return [self.__config["exec"], "-rtlvH", "--delete-after", "--partial", "--delay-updates", "--safe-links", url, path]
+        return [
+            self.__config["exec"], "-rtlvH", "--delete-after", "--partial",
+            "--delay-updates", "--safe-links", url, path
+        ]
--- server/http.py
+++ server/http.py
@@ -103,8 +103,66 @@ class RequestHandlerTemplate(http.server.SimpleHTTPRequestHandler):
                  'content="text/html; charset=%s">' % enc)
         r.append('<title>%s</title>\n</head>' % title)
         r.append('<body>\n<h1>%s</h1>' % title)
-        r.append('<hr>\n<ul>')
+        r.append('<hr>\n<table border=\"0\">\n')
+        r.append('<tr><th>Distro</th><th>Source</th></tr>\n')
         for name in self._distros:
+            fullname = os.path.join(path, name)
+            displayname = linkname = name
+            # Append / for directories or @ for symbolic links
+            if os.path.isdir(fullname):
+                displayname = name + "/"
+                linkname = name + "/"
+
+            r.append('<tr><td><a href="%s">%s</a></td><td></td>%s</tr>\n' %
+                     (urllib.parse.quote(linkname, errors='surrogatepass'),
+                      html.escape(displayname, quote=False),
+                      config.config.distro_url(name)))
+
+        r.append('</table>\n<hr>\n</body>\n</html>\n')
+        encoded = '\n'.join(r).encode(enc, 'surrogateescape')
+        f = io.BytesIO()
+        f.write(encoded)
+        f.seek(0)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=%s" % enc)
+        self.send_header("Content-Length", str(len(encoded)))
+        self.end_headers()
+        return f
+
+    def list_directory(self, path):
+        """Helper to produce a directory listing (absent index.html).
+
+        Return value is either a file object, or None (indicating an
+        error).  In either case, the headers are sent, making the
+        interface the same as for send_head().
+
+        """
+        try:
+            list = os.listdir(path)
+        except OSError:
+            self.send_error(HTTPStatus.NOT_FOUND,
+                            "No permission to list directory")
+            return None
+        list.sort(key=lambda a: a.lower())
+        r = []
+        try:
+            displaypath = urllib.parse.unquote(self.path,
+                                               errors='surrogatepass')
+        except UnicodeDecodeError:
+            displaypath = urllib.parse.unquote(path)
+        displaypath = html.escape(displaypath, quote=False)
+        enc = sys.getfilesystemencoding()
+        title = 'Directory listing for %s' % displaypath
+        r.append('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" '
+                 '"http://www.w3.org/TR/html4/strict.dtd">')
+        r.append('<html>\n<head>')
+        r.append('<meta http-equiv="Content-Type" '
+                 'content="text/html; charset=%s">' % enc)
+        r.append('<title>%s</title>\n</head>' % title)
+        r.append('<body>\n<h1>%s</h1>' % title)
+        r.append('<hr>\n<table border=\"0\">\n')
+        r.append('<tr><th>Name</th><th>Size</th><th>Last Modified</th></tr>\n')
+        for name in list:
             fullname = os.path.join(path, name)
             displayname = linkname = name
             # Append / for directories or @ for symbolic links
@@ -114,15 +172,19 @@ class RequestHandlerTemplate(http.server.SimpleHTTPRequestHandler):
             if os.path.islink(fullname):
                 displayname = name + "@"
                 # Note: a link to a directory displays with @ and links with /
-            r.append('<li><a href="%s">%s</a></li>' %
-                     (urllib.parse.quote(linkname, errors='surrogatepass'),
-                      html.escape(displayname, quote=False)))
-        r.append('</ul>\n<hr>\n</body>\n</html>\n')
+
+            fs = os.stat(fullname)
+            r.append(
+                '<tr><td><a href="%s">%s</a></td><td>%d</td><td>%s</td></tr>\n'
+                % (urllib.parse.quote(linkname, errors='surrogatepass'),
+                   html.escape(displayname, quote=False), fs[6],
+                   self.date_time_string(fs.st_mtime)))
+        r.append('</table>\n<hr>\n</body>\n</html>\n')
         encoded = '\n'.join(r).encode(enc, 'surrogateescape')
         f = io.BytesIO()
         f.write(encoded)
         f.seek(0)
-        self.send_response(200)
+        self.send_response(HTTPStatus.OK)
         self.send_header("Content-type", "text/html; charset=%s" % enc)
         self.send_header("Content-Length", str(len(encoded)))
         self.end_headers()
