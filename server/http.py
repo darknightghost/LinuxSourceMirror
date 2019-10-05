93a2904c38ac81a5c5242f6a46a8fb49675c9b74
wang_siyuan <darknightghost@gmail.com>
2019-10-05 09:44:41 +0800

    UTC:2019-10-05  1:44:41 : test

--- server/http.py
+++ server/http.py
@@ -56,9 +56,14 @@ class RequestHandlerTemplate(http.server.SimpleHTTPRequestHandler):
                         f = open(path, 'rb')
                         try:
                             fs = os.fstat(f.fileno())
+                            pos = 0
+                            size = fs[6]
+                            print(self.requestline)
                             self.send_response(200)
                             self.send_header("Content-type", ctype)
-                            self.send_header("Content-Length", str(fs[6]))
+                            self.send_header("Content-Range",
+                                             "%d-%d" % (pos, pos + size - 1))
+                            self.send_header("Content-Length", str(size))
                             self.send_header(
                                 "Last-Modified",
                                 self.date_time_string(fs.st_mtime))
