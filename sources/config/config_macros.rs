6cf4544e3b45cabd64fef8eed19cc28375f1498f
wang_siyuan <darknightghost@gmail.com>
2022-02-05 02:33:28 +0800

    UTC:2022-02-04 18:33:28 : macro

--- sources/config/config_macros.rs
+++ sources/config/config_macros.rs
@@ -16,6 +16,59 @@ struct AttributeArgument {
     value: Box<dyn::std::any::Any>,
 }
 
+impl ::std::fmt::Display for AttributeArgument {
+    fn fmt(&self, formatter: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        // Key.
+        let mut key_str = "None".to_string();
+        if let Option::Some(ref key) = self.key {
+            key_str = ::std::format!("\"{}\"", key);
+        }
+
+        // Value.
+        let mut value_str = "Unknow Type".to_string();
+        if self.value.is::<String>() {
+            let val_ref: &String = self.value.downcast_ref::<String>().unwrap();
+            value_str = ::std::format!("\"{}\"", val_ref);
+        } else if self.value.is::<i64>() {
+            let val_ref: &i64 = self.value.downcast_ref::<i64>().unwrap();
+            value_str = ::std::format!("{}", val_ref);
+        }
+
+        return write!(formatter, "key: {}, value: {}", key_str, value_str);
+    }
+}
+
+/// Parse value type.
+///
+/// # Arguments
+///
+/// * `value_str` - Value string.
+fn parse_value_str(value_str: String) -> Box<dyn::std::any::Any> {
+    // Is integer?
+    let exp = ::regex::Regex::new("^([+-]?\\d+)$").unwrap();
+    let cap_result = exp.captures(value_str.as_str());
+    if let Option::Some(cap) = cap_result {
+        let ret: i64 = cap
+            .get(1)
+            .unwrap()
+            .as_str()
+            .to_string()
+            .parse::<i64>()
+            .unwrap();
+        return Box::new(ret);
+    }
+
+    // Is string?
+    let exp = ::regex::Regex::new("^\"(.*)\"$").unwrap();
+    let cap_result = exp.captures(value_str.as_str());
+    if let Option::Some(cap) = cap_result {
+        let ret: String = cap.get(1).unwrap().as_str().to_string();
+        return Box::new(ret);
+    }
+
+    panic!("Unsupported format!");
+}
+
 /// Split arguments.
 ///
 /// # Arguments
@@ -99,21 +152,27 @@ fn parse_attr_args(input: ::proc_macro::TokenStream) -> Vec<AttributeArgument> {
 
     // Parse.
     let key_value_exp = ::regex::Regex::new("^([_a-zA-Z][_0-9a-zA-Z]*)\\s*=\\s*(.*)$").unwrap();
-    let ret: Vec<AttributeArgument> = Vec::new();
+    let mut ret: Vec<AttributeArgument> = Vec::new();
     let splitted = split_arguments(&input_str);
     for argument_str in splitted.iter() {
         let key_value_captured = key_value_exp.captures(argument_str.as_str());
         if let Option::Some(cap) = key_value_captured {
             let key = cap.get(1).unwrap().as_str().to_string();
             let value = cap.get(2).unwrap().as_str().to_string();
-            println!("key = {}, value = {}", key, value);
+            ret.push(AttributeArgument {
+                key: Option::Some(key),
+                value: parse_value_str(value),
+            });
         } else {
-            let value = argument_str;
-            println!("value = {}", value);
+            let value = argument_str.to_string();
+            ret.push(AttributeArgument {
+                key: Option::None,
+                value: parse_value_str(value),
+            });
         }
     }
 
-    return Vec::new();
+    return ret;
 }
 
 #[proc_macro_attribute]
@@ -138,8 +197,9 @@ pub fn config_struct(
                         if attr_name.to_string() == "config_field" {
                             println!("---------------------------");
                             let args = parse_attr_args(attr.tokens.clone().into());
-
-                            println!("{}", attr.tokens);
+                            for a in args.iter() {
+                                println!("{}", a);
+                            }
                             println!("---------------------------");
                         } else {
                             new_attrs.push(attr.clone());
